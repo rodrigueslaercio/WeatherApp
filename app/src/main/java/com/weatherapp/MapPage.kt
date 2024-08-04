618dce61d27a4070494ce9ffc10f992746c83a18
rodrigueslaercio <laercio.rodrigs@gmail.com>
2024-08-04 11:18:21 -0300

    Prática 02 - Parte 01 - Criando as novas telas

--- app/src/main/java/com/weatherapp/MapPage.kt
+++ app/src/main/java/com/weatherapp/MapPage.kt
@@ -1,40 +1,30 @@
 package com.weatherapp
 
-import android.app.Activity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
-fun HomePage() {
+fun MapPage() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(colorResource(id = R.color.teal_700))
+            .background(colorResource(id = R.color.purple_700))
             .wrapContentSize(Alignment.Center)
     ) {
         Text(
-            text = "Home",
+            text = "Mapa",
             fontWeight = FontWeight.Bold,
             modifier = Modifier.align(Alignment.CenterHorizontally),
             textAlign = TextAlign.Center,
