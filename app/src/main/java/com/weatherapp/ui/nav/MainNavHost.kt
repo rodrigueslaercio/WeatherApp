603f908d72c19e128ddec6a217f5c45b50df0dea
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2025-06-08 21:54:33 -0300

    Prática 02 - Parte 2

---
+++ app/src/main/java/com/weatherapp/ui/nav/MainNavHost.kt
@@ -0,0 +1,18 @@
+package com.weatherapp.ui.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.weatherapp.ui.HomePage
+import com.weatherapp.ui.ListPage
+import com.weatherapp.ui.MapPage
+
+@Composable
+fun MainNavHost(navController: NavHostController) {
+    NavHost(navController, startDestination = Route.Home) {
+        composable<Route.Home> { HomePage() }
+        composable<Route.List> { ListPage() }
+        composable<Route.Map> { MapPage() }
+    }
+}
