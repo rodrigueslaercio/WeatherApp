9c9351be26ec98d8c085769cbc52acbbbafad06d
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2025-06-29 14:31:54 -0300

    Prática 04 - Parte 3

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -2,6 +2,7 @@ package com.weatherapp.ui
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.weatherapp.model.MainViewModel
 
 @SuppressLint("ContextCastToActivity")
@@ -27,13 +34,45 @@ fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
     Column(
         modifier = Modifier.fillMaxSize().background(Color.Gray).wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+       val recife = LatLng(-8.05, -34.9)
+       val caruaru = LatLng(-8.27, -35.98)
+       val joaopessoa = LatLng(-7.12, -34.84)
+       val camPosState = rememberCameraPositionState()
+       GoogleMap(modifier = Modifier.fillMaxSize(), onMapClick = {
+           viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
+       }, cameraPositionState = camPosState) {
+           viewModel.cities.forEach{
+               if (it.location != null) {
+                   Marker(state = MarkerState(position = it.location),
+                       title = it.name, snippet = "${it.location}"
+                   )
+               }
+           }
+           Marker(
+               state = MarkerState(position = recife),
+               title = "Recife",
+               snippet = "Marcador em Recife",
+               icon = BitmapDescriptorFactory.defaultMarker(
+                   BitmapDescriptorFactory.HUE_BLUE
+               )
+           )
+           Marker(
+               state = MarkerState(position = caruaru),
+               title = "Caruaru",
+               snippet = "Marcador em Caruaru",
+               icon = BitmapDescriptorFactory.defaultMarker(
+                   BitmapDescriptorFactory.HUE_RED
+               )
+           )
+           Marker(
+               state = MarkerState(position = joaopessoa),
+               title = "João Pessoa",
+               snippet = "Marcador em João Pessoa",
+               icon = BitmapDescriptorFactory.defaultMarker(
+                   BitmapDescriptorFactory.HUE_GREEN
+               )
+
+           )
+       }
     }
 }
