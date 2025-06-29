7457a71abd3fbe0a247de764f0bc6c0c81a285c7
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2025-06-29 14:37:08 -0300

    Prática 04 - Parte 4

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -1,7 +1,9 @@
 package com.weatherapp.ui
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -11,6 +13,9 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,9 +23,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -33,14 +41,22 @@ fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
     val activity = LocalContext.current as? Activity
     Column(
         modifier = Modifier.fillMaxSize().background(Color.Gray).wrapContentSize(Alignment.Center)
-    ) {
-       val recife = LatLng(-8.05, -34.9)
-       val caruaru = LatLng(-8.27, -35.98)
-       val joaopessoa = LatLng(-7.12, -34.84)
-       val camPosState = rememberCameraPositionState()
+    ) { val recife = LatLng(-8.05, -34.9)
+        val caruaru = LatLng(-8.27, -35.98)
+        val joaopessoa = LatLng(-7.12, -34.84)
+        val camPosState = rememberCameraPositionState()
+        val context = LocalContext.current
+        val hasLocationPermission by remember {
+            mutableStateOf(
+                ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            )
+        }
        GoogleMap(modifier = Modifier.fillMaxSize(), onMapClick = {
            viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
-       }, cameraPositionState = camPosState) {
+       }, cameraPositionState = camPosState, properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+           uiSettings = MapUiSettings(myLocationButtonEnabled = true)
+       ) {
            viewModel.cities.forEach{
                if (it.location != null) {
                    Marker(state = MarkerState(position = it.location),
