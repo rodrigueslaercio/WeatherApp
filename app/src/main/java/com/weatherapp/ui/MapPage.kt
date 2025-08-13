05c175b5a388e828cf8049d3787a2e57f79ecd89
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2025-08-13 20:09:53 -0300

    Prática 09 - Parte 2

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -25,6 +25,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.scale
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -33,6 +36,7 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.weatherapp.R
 import com.weatherapp.model.MainViewModel
 
 @SuppressLint("ContextCastToActivity")
@@ -56,14 +60,36 @@ fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
            uiSettings = MapUiSettings(myLocationButtonEnabled = true)
        ) {
            viewModel.cities.forEach{
+//               if (it.location != null) {
+//                   LaunchedEffect(it.name) {
+//                       if (it.weather == null) {
+//                           viewModel.loadWeather(it.name)
+//                       }
+//                   }
+//                   Marker(state = MarkerState(position = it.location),
+//                       title = it.name, snippet = it.weather?.desc?:"Carregando..."
+//                   )
+//               }
                if (it.location != null) {
                    LaunchedEffect(it.name) {
                        if (it.weather == null) {
                            viewModel.loadWeather(it.name)
                        }
                    }
-                   Marker(state = MarkerState(position = it.location),
-                       title = it.name, snippet = it.weather?.desc?:"Carregando..."
+                   LaunchedEffect(it.weather) {
+                       if (it.weather != null && it.weather!!.bitmap == null) {
+                           viewModel.loadBitmap(it.name)
+                       }
+                   }
+                   val image = it.weather?.bitmap ?:
+                   getDrawable(context, R.drawable.loading)!!
+                       .toBitmap()
+                   val marker = BitmapDescriptorFactory
+                       .fromBitmap(image.scale(120,120))
+                   Marker( state = MarkerState(position = it.location),
+                       title = it.name,
+                       icon = marker,
+                       snippet = it.weather?.desc?:"Carregando..."
                    )
                }
            }
