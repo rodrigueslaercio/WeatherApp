44c3211a4dfc80a4df321776b28f21d1a85ca5f3
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2025-08-31 22:10:51 -0300

    Prática 12 - Parte 1

--- app/src/main/java/com/weatherapp/db/fb/FBDatabase.kt
+++ app/src/main/java/com/weatherapp/db/fb/FBDatabase.kt
@@ -5,7 +5,11 @@ import com.google.firebase.auth.auth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.snapshots
 import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.map
 
 class FBDatabase {
     interface Listener {
@@ -18,41 +22,60 @@ class FBDatabase {
 
     private val auth = Firebase.auth
     private val db = Firebase.firestore
+
+    val user: Flow<FBUser>
+        get() {
+            if (auth.currentUser == null) return emptyFlow()
+            return db.collection("users")
+                .document(auth.currentUser!!.uid)
+                .snapshots().map { it.toObject(FBUser::class.java)!! }
+        }
+    val cities: Flow<List<FBCity>>
+        get() {
+            if (auth.currentUser == null) return emptyFlow()
+            return db.collection("users")
+                .document(auth.currentUser!!.uid)
+                .collection("cities")
+                .snapshots().map {
+                        snapshot -> snapshot.toObjects(FBCity::class.java)
+                }
+        }
+
     private var citiesListReg: ListenerRegistration? = null
     private var listener : Listener? = null
 
-    init {
-        auth.addAuthStateListener { auth ->
-            if (auth.currentUser == null) {
-                citiesListReg?.remove()
-                listener?.onUserSignOut()
-                return@addAuthStateListener
-            }
-
-            val refCurrUser = db.collection("users").document(auth.currentUser!!.uid)
-            refCurrUser.get().addOnSuccessListener {
-                it.toObject(FBUser::class.java)?.let { user ->
-                    listener?.onUserLoaded(user)
-                }
-            }
-
-            citiesListReg = refCurrUser.collection("cities")
-                .addSnapshotListener { snapshots, ex ->
-                    if (ex != null) return@addSnapshotListener
-
-                    snapshots?.documentChanges?.forEach { change ->
-                        val fbCity = change.document.toObject(FBCity::class.java)
-                        if (change.type == DocumentChange.Type.ADDED) {
-                            listener?.onCityAdded(fbCity)
-                        } else if (change.type == DocumentChange.Type.MODIFIED) {
-                            listener?.onCityUpdated(fbCity)
-                        } else if (change.type == DocumentChange.Type.REMOVED) {
-                            listener?.onCityRemoved(fbCity)
-                        }
-                    }
-                }
-        }
-    }
+//    init {
+//        auth.addAuthStateListener { auth ->
+//            if (auth.currentUser == null) {
+//                citiesListReg?.remove()
+//                listener?.onUserSignOut()
+//                return@addAuthStateListener
+//            }
+//
+//            val refCurrUser = db.collection("users").document(auth.currentUser!!.uid)
+//            refCurrUser.get().addOnSuccessListener {
+//                it.toObject(FBUser::class.java)?.let { user ->
+//                    listener?.onUserLoaded(user)
+//                }
+//            }
+//
+//            citiesListReg = refCurrUser.collection("cities")
+//                .addSnapshotListener { snapshots, ex ->
+//                    if (ex != null) return@addSnapshotListener
+//
+//                    snapshots?.documentChanges?.forEach { change ->
+//                        val fbCity = change.document.toObject(FBCity::class.java)
+//                        if (change.type == DocumentChange.Type.ADDED) {
+//                            listener?.onCityAdded(fbCity)
+//                        } else if (change.type == DocumentChange.Type.MODIFIED) {
+//                            listener?.onCityUpdated(fbCity)
+//                        } else if (change.type == DocumentChange.Type.REMOVED) {
+//                            listener?.onCityRemoved(fbCity)
+//                        }
+//                    }
+//                }
+//        }
+//    }
 
     fun setListener(listener: Listener? = null) {
         this.listener = listener
